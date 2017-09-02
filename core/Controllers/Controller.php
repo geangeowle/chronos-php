f5f8eb8356e8bac303dd17a8a1d2819625f1169c
Gean Geowle <geangeowle@gmail.com>
2017-09-01 22:49:26 -0300

    Add controller

---
+++ core/Controllers/Controller.php
@@ -0,0 +1,31 @@
+<?php
+namespace Chronos\Controllers;
+
+use Chronos\Utils\Inflector;
+use Chronos\Views\View;
+
+class Controller
+{
+
+    public $name = "";
+    public $viewPath = null;
+    public $output = null;
+
+    public function __construct(){
+        if ($this->viewPath === null) {
+            $this->viewPath = Inflector::underscore($this->name);
+        }
+    }
+
+    public function render($action = null, $layout = null, $file = null)
+    {
+        //$this->beforeRender();
+        //$viewClass = $this->view;
+        $viewClass = "Chronos\Views\View";
+        $view = new $viewClass($this);
+        $this->autoRender = false;
+        $this->output .= $view->render($action, $layout, $file);
+        return $this->output;
+    }
+
+}
