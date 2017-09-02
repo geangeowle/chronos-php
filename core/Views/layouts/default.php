5a2662b0e2b5398f1421b868e50a0c9b579a547d
Gean Geowle <geangeowle@gmail.com>
2017-09-01 22:47:57 -0300

    added file to main layout

---
+++ core/Views/layouts/default.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title><?php echo $title_for_layout; ?></title>
+  <meta name="description" content="ChronosPHP">
+  <meta name="author" content="Gean Geowle">
+
+  <!-- <link rel="stylesheet" href="css/styles.css?v=1.0"> -->
+
+</head>
+
+<body>
+	<div style="margin: 60px auto; width: 520px; border:1px solid #f4f4f4; text-align: center; font-family: sans-serif; padding: 10px;">
+		<?php echo $content_for_layout; ?>
+	</div>
+	<!-- <script src="js/scripts.js"></script> -->
+</body>
+</html>
