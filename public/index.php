b656ad211eadc27ed6b52d56a6a1b6d4ba472384
Gean Geowle <geangeowle@gmail.com>
2017-09-02 20:56:15 -0300

    Add test BASE_DIR

--- public/index.php
+++ public/index.php
@@ -4,8 +4,10 @@
     error_reporting(E_ALL);
     date_default_timezone_set("America/Sao_Paulo");
 
+    define("BASE_DIR", dirname(__DIR__));
+
     use Chronos\Chronos;
     require_once dirname(__DIR__) . "/vendor/autoload.php";
-
+    //print_r(__DIR__);
     $app = Chronos::getInstance();
     $app->run();
