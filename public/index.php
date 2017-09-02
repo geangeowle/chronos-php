6c354905a3c1f542ad89e02bb1767585db655542
Gean Geowle <geangeowle@gmail.com>
2017-09-01 22:40:31 -0300

    Add error_reporting

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
-  use Chronos\Chronos;
-  require_once dirname(__DIR__) . "/vendor/autoload.php";
+    ini_set("display_errors", "1");
+    error_reporting(E_ALL);
+    date_default_timezone_set("America/Sao_Paulo");
 
-  $app = Chronos::getInstance();
-  $app->run();
+    use Chronos\Chronos;
+    require_once dirname(__DIR__) . "/vendor/autoload.php";
+
+    $app = Chronos::getInstance();
+    $app->run();
