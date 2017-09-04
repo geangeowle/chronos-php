b0a187926c561f82177752ca920e7a245ef2ad99
Gean Geowle <geangeowle@gmail.com>
2017-09-04 11:18:33 -0300

    Removed getInstance method

--- public/index.php
+++ public/index.php
@@ -4,10 +4,12 @@
     error_reporting(E_ALL);
     date_default_timezone_set("America/Sao_Paulo");
 
-    define("BASE_DIR", dirname(__DIR__));
-
     use Chronos\Chronos;
-    require_once dirname(__DIR__) . "/vendor/autoload.php";
-    //print_r(__DIR__);
-    $app = Chronos::getInstance();
-    $app->run();
+    require_once(dirname(__DIR__) . "/vendor/autoload.php");
+
+    $appConfig = require_once (dirname(__DIR__) . "/app/Config/ConfigPaths.php");
+    // $app = Chronos::getInstance();
+    // $app->setConfig($appConfig);
+    // $app->run();
+
+    (new Chronos($appConfig))->run();
