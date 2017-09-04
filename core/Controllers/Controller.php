89cf5ff197a320bf16a743c1bc956a25a21b921d
Gean Geowle <geangeowle@gmail.com>
2017-09-04 13:33:13 -0300

    Removed setConfig method

--- core/Controllers/Controller.php
+++ core/Controllers/Controller.php
@@ -1,16 +1,16 @@
 <?php
 namespace Chronos\Controllers;
 
+use Chronos\Base\App;
 use Chronos\Utils\Inflector;
 use Chronos\Views\View;
 
-class Controller
+class Controller extends App
 {
 
     public $name = "";
     public $viewPath = null;
     public $output = null;
-    private $appConfig = array();
 
     public function __construct(){
         if ($this->viewPath === null) {
@@ -18,16 +18,14 @@ class Controller
         }
     }
 
-    public function setConfig($newAppConfig = array()){
-      $this->appConfig = $newAppConfig;
-    }
-
     public function render($action = null, $layout = null, $file = null)
     {
         //$this->beforeRender();
         //$viewClass = $this->view;
         $viewClass = "Chronos\Views\View";
+
         $view = new $viewClass($this);
+
         $this->autoRender = false;
         $this->output .= $view->render($action, $layout, $file);
         return $this->output;
