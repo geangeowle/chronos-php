bd8c53aa86785bc302da18bd26adfe6129566193
Gean Geowle <geangeowle@gmail.com>
2017-09-04 13:33:31 -0300

    Sync paths

--- app/Config/ConfigPaths.php
+++ app/Config/ConfigPaths.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-  "APP_PATH" => dirname(__DIR__) . "/app",
+  "APP_PATH" => dirname(dirname(__DIR__)) . "/app",
   "CHRONOS_PATH" => dirname(dirname(__DIR__)) . "/core"
   #"CHRONOS_PATH" => dirname(dirname(__DIR__)) . "/vendor/geangeowle/chronos-php/core"
 );
