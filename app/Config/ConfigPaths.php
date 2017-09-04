f3cc3b5b7e6bb975399dd7ec2dfd4ee840ac098e
Gean Geowle <geangeowle@gmail.com>
2017-09-04 11:17:59 -0300

    Add config paths

---
+++ app/Config/ConfigPaths.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+  "APP_PATH" => dirname(__DIR__) . "/app",
+  "CHRONOS_PATH" => dirname(dirname(__DIR__)) . "/core"
+  #"CHRONOS_PATH" => dirname(dirname(__DIR__)) . "/vendor/geangeowle/chronos-php/core"
+);
