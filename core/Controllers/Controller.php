ae36d59a845ad584f14c046ec4e434d8cd875676
Gean Geowle <geangeowle@gmail.com>
2017-09-04 11:19:24 -0300

    Add setConfig method

--- core/Controllers/Controller.php
+++ core/Controllers/Controller.php
@@ -10,11 +10,16 @@ class Controller
     public $name = "";
     public $viewPath = null;
     public $output = null;
+    private $appConfig = array();
 
     public function __construct(){
         if ($this->viewPath === null) {
             $this->viewPath = Inflector::underscore($this->name);
         }
+    }
+
+    public function setConfig($newAppConfig = array()){
+      $this->appConfig = $newAppConfig;
     }
 
     public function render($action = null, $layout = null, $file = null)
