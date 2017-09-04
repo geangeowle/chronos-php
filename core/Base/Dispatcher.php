fac9cc61bc2781438657dbd827b510cd390033e3
Gean Geowle <geangeowle@gmail.com>
2017-09-04 13:30:43 -0300

    Sync tabs

--- core/Base/Dispatcher.php
+++ core/Base/Dispatcher.php
@@ -5,47 +5,42 @@ use Chronos\Http\Router;
 use Chronos\Utils\Inflector;
 use Chronos\Base\App;
 
-final class Dispatcher
+final class Dispatcher extends App
 {
-  public $url = null;
-  private $params = null;
-  private $appConfig = array();
+    public $url = null;
+    private $params = null;
 
-  public function __construct($url = null)
-  {
-    $this->url = (isset($_GET["url"])) ? $_GET["url"] : $url;
-  }
+    public function __construct($url = null)
+    {
+        $this->url = (isset($_GET["url"])) ? $_GET["url"] : $url;
+    }
 
-  public function setConfig($newAppConfig = array()){
-    $this->appConfig = $newAppConfig;
-  }
+    public function dispatch()
+    {
+        $this->params = Router::parse($this->url);
+        $objController = $this->__getController();
+        $objController->params = $this->params;
+        $objController->setConfig($this->getConfig());
+        return $this->_invoke($objController, $this->params);
+    }
 
-  public function dispatch()
-  {
-      $this->params = Router::parse($this->url);
-      $objController = $this->__getController();
-      $objController->params = $this->params;
-      $objController->appConfig = $this->appConfig;
-      return $this->_invoke($objController, $this->params);
-  }
+    private function __getController(){
+        $ctrlClass = "Chronos\\Controllers\\" . $this->__loadController($this->params);
+        $objController = new $ctrlClass();
+        return $objController;
+    }
 
-  private function __getController(){
-    $ctrlClass = "Chronos\\Controllers\\" . $this->__loadController($this->params);
-    $objController = new $ctrlClass();
-    return $objController;
-  }
+    private function __loadController($params){
+        $name = $params["params"]["controller"];
+        $this->import("Controller", $name);
+        return Inflector::camelize($name . "_controller");
+    }
 
-  private function __loadController($params){
-    $name = $params["params"]["controller"];
-    App::import("Controller", $name);
-    return Inflector::camelize($name . "_controller");
-  }
-
-  public function _invoke(&$controller, $params)
-  {
-      $controller->output = $controller->render($params["params"]["action"]);
-      if (isset($controller->output)) {
-          echo ($controller->output);
-      }
-  }
+    public function _invoke(&$controller, $params)
+    {
+        $controller->output = $controller->render($params["params"]["action"]);
+        if (isset($controller->output)) {
+            echo ($controller->output);
+        }
+    }
 }
