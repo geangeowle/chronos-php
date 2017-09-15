6ca759bde5da31c919bdfce41842c2058f12b278
Gean Geowle <geangeowle@gmail.com>
2017-09-15 17:39:07 -0300

    Added first demo test

---
+++ tests/ChronosDemoTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversNothing
+ */
+class ChronosDemoTest extends TestCase
+{
+    public function testDemo()
+    {
+        $this->assertSame(0, 5 - 5);
+    }
+}
