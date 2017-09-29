f2964c9a3eed639dbf82925f692fcfd50d16691f
Gean Geowle <geangeowle@gmail.com>
2017-09-29 16:12:20 -0300

    Added test of typescript

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,12 +20,47 @@ module.exports = function(grunt) {
                     }
                 }
             }
+        },
+        typescript: {
+            options: {
+                module: 'commonjs',
+                target: 'es5',
+                removeComments: true
+            },
+            dist: {
+                src: 'app/public/assets/ts/*.ts',
+                dest: 'public/assets/js/',
+            },
+        },
+        uglify: {
+            options: {
+                sourceMap: true,
+                compress: {
+                    drop_console: true
+                },
+            },
+            my_target: {
+                files: [{
+                    expand: true,
+                    src: ['public/assets/js/*.js', '!public/assets/js/*.min.js'],
+                    dest: '.',
+                    cwd: '.',
+                    rename: function (dst, src) {
+                        // To keep the source js files and make new files as `*.min.js`:
+                        return dst + '/' + src.replace('.js', '.min.js');
+                        // Or to override to src:
+                        return src;
+                    }
+                }]
+            }
         }
     });
 
     // Load the plugin that provides the "changelog" task.
     grunt.loadNpmTasks('grunt-changelog');
+    grunt.loadNpmTasks('grunt-typescript-compile');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-    grunt.registerTask('default', ['changelog']);
+    grunt.registerTask('default', ['changelog', 'typescript', 'uglify']);
 };
