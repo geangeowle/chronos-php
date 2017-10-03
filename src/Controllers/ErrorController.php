6fa2f0389c7011beb16b52c487bf2febc4cdeaf2
Gean Geowle <geangeowle@gmail.com>
2017-10-03 18:04:40 -0300

    Added index method

--- src/Controllers/ErrorController.php
+++ src/Controllers/ErrorController.php
@@ -2,11 +2,17 @@
 
 namespace Chronos\Controllers;
 
+use App\Controllers\AppController;
+
 final class ErrorController extends AppController
 {
     public $name = 'Error';
     public $layout = 'default_error';
     public $pageTitle = 'ChronosPHP Error';
+
+    public function index(){
+
+    }
 
     public function missingClass($className = '')
     {
