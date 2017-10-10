cce19c0309b3fe6172d2cc244eff925f59127b0a
Gean Geowle <geangeowle@gmail.com>
2017-10-10 16:36:47 -0300

    Removed loadModel method

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -5,15 +5,12 @@ namespace Chronos\Controllers;
 use Chronos\Base\App;
 use Chronos\Utils\Inflector;
 
-//use Chronos\Models\Model as Model;
-
 class Controller extends App
 {
     public $name = '';
     public $pageTitle = '';
     public $viewPath;
     public $output;
-    public $uses = [];
 
     public function __construct($newConfig = [])
     {
@@ -21,21 +18,6 @@ class Controller extends App
         if (null === $this->viewPath) {
             $this->viewPath = Inflector::underscore($this->name);
         }
-        //$this->loadModel($this->uses[0]);
-        // $s = 'Chronos\\Models\\Model';
-        // $t = new Model();
-    }
-
-    public function loadModel($modelName)
-    {
-        // pr($modelName);
-        $nameCamelize = Inflector::camelize($modelName);
-        //$nameCamelize = "{$nameCamelize}Model";
-        // pr($this->import('Model', $nameCamelize));
-        // $this->Error->
-        // $this->ErrorModel->
-        // pr($nameCamelize);
-        // die("");
     }
 
     public function render($action = null, $layout = null, $file = null)
