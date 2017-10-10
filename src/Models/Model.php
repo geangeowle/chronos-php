4661996e7bed4c3998ab2f4df72e98d2e50681e3
Gean Geowle <geangeowle@gmail.com>
2017-10-10 16:34:42 -0300

    Added find method

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -6,8 +6,28 @@ use Chronos\Base\App;
 
 class Model extends App
 {
+    public $useDbConfig = 'default';
+
     public function __construct()
     {
         //pr('....');
+    }
+
+    public function find($type, $options = [])
+    {
+        pr('----begin -> find');
+        pr($type);
+        pr($options);
+
+        pr('----end -> find');
+
+        return [];
+    }
+
+    private function _execute($querySQL)
+    {
+        pr($querySQL);
+
+        return [];
     }
 }
