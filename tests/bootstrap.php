6346393d0032ce7e4ab2bca93053846c3d2d8f6c
Gean Geowle <geangeowle@gmail.com>
2017-10-10 16:38:50 -0300

    Added conf, basics

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,7 @@
     error_reporting(E_ALL);
     date_default_timezone_set('America/Sao_Paulo');
 
+    define('APP_CONFIG', require_once dirname(__DIR__).'/app/Config/ConfigPaths.php');
+
     require_once dirname(__DIR__).'/vendor/autoload.php';
+    require_once dirname(__DIR__).'/src/basics.php';
