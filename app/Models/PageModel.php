0fd11a03d1f6ac7f0c7221e3af8dc43360b6875d
Gean Geowle <geangeowle@gmail.com>
2017-10-18 17:51:41 -0200

    Added test find

--- app/Models/PageModel.php
+++ app/Models/PageModel.php
@@ -4,13 +4,23 @@ namespace App\Models;
 
 class PageModel extends AppModel
 {
+    public $name = 'Page';
+    public $useTable = 'tb_page';
+
     public function r()
     {
         //$n = (new PageTestModel())->r();
         //$
         //$
 
-        return $this->find('first', ['conditions' => []]);
+        return $this->find('all', [
+            'conditions' => [
+                'Page.id IN (1, 3, 4, 6)',
+            ],
+            'fields' => [
+                'Page.id', 'Page.ds',
+            ],
+        ]);
         // pr($this->getConfig());
         // pr('..PageModel..');
     }
