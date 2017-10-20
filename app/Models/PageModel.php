92c5a0f34f76189ee19904990763aab4879e2820
Gean Geowle <geangeowle@gmail.com>
2017-10-20 18:23:01 -0200

    Sync

--- app/Models/PageModel.php
+++ app/Models/PageModel.php
@@ -6,6 +6,7 @@ class PageModel extends AppModel
 {
     public $name = 'Page';
     public $useTable = 'tb_page';
+    public $pk = 'id';
 
     public function r()
     {
@@ -13,14 +14,50 @@ class PageModel extends AppModel
         //$
         //$
 
-        return $this->find('all', [
+        $dadosSave = [
+            // 'id' => '12',
+            'ds' => 'rr',
+            // 'dt' => null,
+            // 'dt' => 'null',
+            // 'ds2' => '',
+            // 'nr' => 0,
+            // 'nr1' => '0',
+            // 'nr2' => -1,
+            // 'fl_true' => true,
+            // 'fl_false' => false,
+        ];
+        // pr($this->save($dadosSave));
+
+        pr('------------');
+        $dadosUpd = [
+            'id_fk' => 'null',
+            'ds' => 'rr',
+            'nr' => 0,
+            'nr1' => '0',
+            'nr2' => -1,
+            'fl_true' => true,
+            'fl_false' => false,
+        ];
+        $where = [
+            'id IN ( 1, 2) ',
+            // "ds IN ('ds', 'dsx')",
+            // "( id = 2 OR ds IN ('y', 'x'))",
+        ];
+        pr($this->save($dadosUpd, $where));
+
+        $dados = $this->find('all', [
             'conditions' => [
-                'Page.id IN (1, 3, 4, 6)',
+                // 'Page.id IN (1, 3, 4, 6)',
             ],
             'fields' => [
                 'Page.id', 'Page.ds',
             ],
+            'order' => [
+                'Page.id DESC',
+            ],
         ]);
+
+        pr($dados);
         // pr($this->getConfig());
         // pr('..PageModel..');
     }
