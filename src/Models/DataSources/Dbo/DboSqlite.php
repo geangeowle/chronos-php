0397b25b30ae5c8d2e1804dd3a1dc3d52d28e5de
Gean Geowle <geangeowle@gmail.com>
2017-10-21 19:41:12 -0200

    Added fetch method

--- src/Models/DataSources/Dbo/DboSqlite.php
+++ src/Models/DataSources/Dbo/DboSqlite.php
@@ -10,6 +10,9 @@ class DboSqlite extends DataSource
     private $description = 'Driver DboSqlite';
     private $extension = 'sqlite3';
 
+    private $connResource;
+    private $results = [];
+
     public function getDescription()
     {
         return sprintf('[%s] %s', $this->extension, $this->description);
@@ -24,7 +27,7 @@ class DboSqlite extends DataSource
     {
         //$dbhandle = sqlite_open('db/test.db', 0666, $error);
         //try {
-        $this->conn = new \SQLite3('/var/www/html/public/test');
+        $this->connResource = new \SQLite3('/var/www/html/public/test');
         // } catch (Exception $e) {
         //     die($e->getMessage());
         // }
@@ -32,22 +35,26 @@ class DboSqlite extends DataSource
 
     public function disconnect()
     {
-        $this->conn->close();
+        $this->connResource->close();
     }
 
     public function query($querySql)
     {
-        $result = $this->conn->query($querySql);
+        $this->results = $this->connResource->query($querySql);
+    }
+
+    public function fetch()
+    {
         $return = [];
         // change this
-        if (false !== $result) {
-            $cols = $result->numColumns();
-            while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+        if (false !== $this->results) {
+            $cols = $this->results->numColumns();
+            while ($row = $this->results->fetchArray(SQLITE3_ASSOC)) {
                 $return[] = $row;
             }
         } else {
             // just for the moment
-            pr($this->conn->lastErrorMsg());
+            pr($this->connResource->lastErrorMsg());
             die("Error in query: <span style='color:red;'>{$querySql}</span>");
         }
 
