9080add737310d82d20666e7028d49d63555f205
Gean Geowle <geangeowle@gmail.com>
2017-10-22 23:16:13 -0200

    Added test of DboSqliteTest

--- tests/src/Models/DataSource/Dbo/DboSqliteTest.php
+++ tests/src/Models/DataSource/Dbo/DboSqliteTest.php
@@ -12,9 +12,30 @@ class DboSqliteTest extends PHPUnit\Framework\TestCase
      *
      * @return [type] [description]
      */
-    public function testEnable()
+    public function testEnableReturnsEnable()
     {
+        $config = [
+            'datasource' => 'dbo',
+            'driver' => 'sqlite',
+            'database' => '/var/www/html/public/test',
+            'prefix' => 'tb_',
+        ];
         $dbo = new DboSqlite();
+        // $dbo->setConfig($config);
         $this->assertTrue($dbo->enable());
+    }
+
+    public function testConnectionReturnsConnected()
+    {
+        $config = [
+            'datasource' => 'dbo',
+            'driver' => 'sqlite',
+            'database' => dirname(dirname(dirname(dirname(dirname(__DIR__))))).'/public/test',
+            'prefix' => 'tb_',
+        ];
+
+        $dbo = new DboSqlite();
+        $dbo->setConfig($config);
+        $this->assertTrue($dbo->connect());
     }
 }
