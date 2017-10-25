cf9c67f1179908c9b507bbc3290bf40634eef97f
Gean Geowle <geangeowle@gmail.com>
2017-10-25 18:08:04 -0200

    Removed setConfig

--- src/Chronos.php
+++ src/Chronos.php
@@ -9,15 +9,9 @@ require_once 'basics.php';
 
 final class Chronos extends BaseObject
 {
-    public function __construct($newAppConfig = [])
-    {
-        $this->setConfig($newAppConfig);
-    }
-
     public function run()
     {
         $objDispatcher = new Dispatcher();
-        $objDispatcher->setConfig($this->getConfig());
         $objDispatcher->dispatch();
     }
 }
