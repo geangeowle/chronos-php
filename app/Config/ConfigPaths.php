7a4d853b5eca21b73710d56b0f8ba1ef0302eca6
Gean Geowle <geangeowle@gmail.com>
2017-10-25 18:03:37 -0200

    Removed file

--- app/Config/ConfigPaths.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-  'APP_PATH' => dirname(dirname(__DIR__)).'/app',
-  'CHRONOS_PATH' => dirname(dirname(__DIR__)).'/src',
-  //"CHRONOS_PATH" => dirname(dirname(__DIR__)) . "/vendor/geangeowle/chronos-php/src"
-];
