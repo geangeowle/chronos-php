11febefd3b5fe55c20d2652dc29067fb7953c900
Gean Geowle <geangeowle@gmail.com>
2017-10-25 18:07:05 -0200

    Added set, setLayout methods

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -10,14 +10,25 @@ class Controller extends App
     public $name = '';
     public $pageTitle = '';
     public $viewPath;
+    public $viewVars = [];
     public $output;
+    private $layout = 'default';
 
-    public function __construct($newConfig = [])
+    public function __construct()
     {
-        $this->setConfig($newConfig);
         if (null === $this->viewPath) {
             $this->viewPath = Inflector::underscore($this->name);
         }
+    }
+
+    public function setLayout($newLayout)
+    {
+        $this->layout = $newLayout;
+    }
+
+    public function getLayout()
+    {
+        return $this->layout;
     }
 
     public function render($action = null, $layout = null, $file = null)
@@ -32,5 +43,10 @@ class Controller extends App
         $this->output .= $view->render($action, $layout, $file);
 
         return $this->output;
+    }
+
+    public function set($var, $value)
+    {
+        $this->viewVars[$var] = $value;
     }
 }
