a472ad567ee38ee831cc63a2a8bfb270a2c9210d
Gean Geowle <geangeowle@gmail.com>
2017-10-25 18:09:34 -0200

    Removed appConfig

--- public/index.php
+++ public/index.php
@@ -5,12 +5,20 @@
     date_default_timezone_set('America/Sao_Paulo');
 
     use Chronos\Chronos;
+    use Chronos\Utils\Configure;
 
     require_once dirname(__DIR__).'/vendor/autoload.php';
+    require_once dirname(__DIR__).'/app/Config/boot.php';
 
-    $appConfig = require_once dirname(__DIR__).'/app/Config/ConfigPaths.php';
+    // echo '<pre>';
+    // var_dump(Configure::read('App.Path'));
+    // var_dump(Configure::read('Chronos.Path'));
+
+    // $appConfig = require_once dirname(__DIR__).'/app/Config/ConfigPaths.php';
     // $app = Chronos::getInstance();
     // $app->setConfig($appConfig);
     // $app->run();
 
-    (new Chronos($appConfig))->run();
+    // (new Chronos($appConfig))->run();
+    //
+    (new Chronos())->run();
