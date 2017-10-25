680e5b9fd551efb6ff546efe12fa1796266b3c22
Gean Geowle <geangeowle@gmail.com>
2017-10-25 18:08:24 -0200

    Removed setConfig

--- src/Base/BaseObject.php
+++ src/Base/BaseObject.php
@@ -2,17 +2,30 @@
 
 namespace Chronos\Base;
 
+use Chronos\Utils\Configure;
+
 class BaseObject
 {
-    private $appConfig = [];
-
-    public function setConfig($newAppConfig = [])
-    {
-        $this->appConfig = $newAppConfig;
-    }
-
-    public function getConfig()
-    {
-        return $this->appConfig;
-    }
+    // private $appConfig = [];
+    //
+    // public function __construct($newAppConfig = [])
+    // {
+    //     if (empty($newAppConfig)) {
+    //         $newAppConfig = [
+    //             'APP_PATH' => Configure::read('App.Path'),
+    //             'CHRONOS_PATH' => Configure::read('Chronos.Path'),
+    //         ];
+    //     }
+    //     $this->setConfig($newAppConfig);
+    // }
+    //
+    // public function setConfig($newAppConfig = [])
+    // {
+    //     $this->appConfig = $newAppConfig;
+    // }
+    //
+    // public function getConfig()
+    // {
+    //     return $this->appConfig;
+    // }
 }
