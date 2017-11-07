073f019f607e82d0c6131b7bef5036c22a3dea39
Gean Geowle <geangeowle@gmail.com>
2017-11-07 17:42:23 -0200

    Added Render for Twig

---
+++ src/Views/Render/RenderTwig.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Chronos\Views\Render;
+
+use Chronos\Utils\Configure;
+use Chronos\Views\BaseRender;
+use Chronos\Views\Form;
+
+class RenderTwig implements BaseRender
+{
+    private $viewVars = [];
+
+    public function setViewVars($viewVars)
+    {
+        $this->viewVars = $viewVars;
+    }
+
+    public function render()
+    {
+        $this->viewVars['title'] = 'ChronosPHP';
+        pr($this->viewVars);
+
+        $viewPath = 'Page';
+        $action = 'Page/index';
+
+        $pathApp = Configure::read('App.Path');
+        $pathCore = Configure::read('Chronos.Path');
+        $pathViewFile = '/Views/'; //.$viewPath.'/'; //.$action.'.php';
+
+        $pathApp = $pathApp.$pathViewFile;
+        $pathCore = $pathCore.$pathViewFile;
+
+        $path = (file_exists($pathApp)) ? $pathApp : $pathCore;
+
+        $loader = new \Twig_Loader_Filesystem([$pathCore, $pathApp]);
+        $twig = new \Twig_Environment($loader, ['debug' => true]);
+        $twig->addExtension(new \Twig_Extension_Debug());
+        $twig->addGlobal('Form', new Form());
+
+        return $twig->render($action.'.html', $this->viewVars);
+    }
+}
