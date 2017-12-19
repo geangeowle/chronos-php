cfa79efa420e33cb07d3b9ddbdc5255d6da8110b
Gean Geowle <geangeowle@gmail.com>
2017-12-18 23:22:53 -0200

    Added test of coverage

--- tests/Unit/src/Utils/InflectorTest.php
+++ tests/Unit/src/Utils/InflectorTest.php
@@ -1,34 +1,15 @@
 <?php
 
+namespace Chronos\Test\Unit\Utils;
+
 use Chronos\Utils\Inflector;
 
-/**
- * @coversNothing
- */
-class InflectorTest extends PHPUnit\Framework\TestCase
+class InflectorTest extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * [testUnderscoreReturnsUnderscoreString description].
-     *
-     * @dataProvider providerTestUnderscoreReturnsUnderscoreString
-     *
-     * @param mixed $originalString
-     * @param mixed $expectedResult
-     *
-     * @return [type] [description]
-     */
-    public function testUnderscoreReturnsUnderscoreString($originalString, $expectedResult)
-    {
-        $result = Inflector::underscore($originalString);
-        $this->assertSame($expectedResult, $result);
-    }
 
-    public function providerTestUnderscoreReturnsUnderscoreString()
+    public function testInflectorUnderscoreReturnsUndercoreString()
     {
-        return [
-            ['TestUnderscoreReturnsUnderscoreString', 'test_underscore_returns_underscore_string'],
-            ['TestUnderscore2ReturnsUnderscoreString2', 'test_underscore2_returns_underscore_string2'],
-            ['123', '123'],
-        ];
+      $this->assertSame(Inflector::underscore('TestInflectorUnderscore'), 'test_inflector_underscore');
+      $this->assertSame(Inflector::humanize('test_inflector_underscore'), 'Test Inflector Underscore');
     }
 }
--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    ini_set('display_errors', '1');
-    error_reporting(E_ALL);
-    date_default_timezone_set('America/Sao_Paulo');
-
-    require_once dirname(__DIR__).'/vendor/autoload.php';
-    require_once dirname(__DIR__).'/app/Config/boot.php';
