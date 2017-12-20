c728b91230c70c8cff82fc0a3893684d34c01e7a
Gean Geowle <geangeowle@gmail.com>
2017-12-19 22:19:54 -0200

    Update InflectorTest

--- tests/Unit/src/Utils/InflectorTest.php
+++ tests/Unit/src/Utils/InflectorTest.php
@@ -12,4 +12,29 @@ class InflectorTest extends \PHPUnit\Framework\TestCase
       $this->assertSame(Inflector::underscore('TestInflectorUnderscore'), 'test_inflector_underscore');
       $this->assertSame(Inflector::humanize('test_inflector_underscore'), 'Test Inflector Underscore');
     }
+
+    /**
+     * [testUnderscoreReturnsUnderscoreString description].
+     *
+     * @dataProvider providerTestUnderscoreReturnsUnderscoreString
+     *
+     * @param mixed $originalString
+     * @param mixed $expectedResult
+     *
+     * @return [type] [description]
+     */
+    public function testUnderscoreReturnsUnderscoreString($originalString, $expectedResult)
+    {
+        $result = Inflector::underscore($originalString);
+        $this->assertSame($expectedResult, $result);
+    }
+
+    public function providerTestUnderscoreReturnsUnderscoreString()
+    {
+        return [
+            ['TestUnderscoreReturnsUnderscoreString', 'test_underscore_returns_underscore_string'],
+            ['TestUnderscore2ReturnsUnderscoreString2', 'test_underscore2_returns_underscore_string2'],
+            ['123', '123'],
+        ];
+    }
 }
