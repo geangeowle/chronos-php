51e5999a582441c45a3429e939f863ca769fd5be
Gean Geowle <geangeowle@gmail.com>
2017-12-28 18:00:12 -0200

    Added method to check methods without implementation of test

--- src/TestSuite/ChronosTestCase.php
+++ src/TestSuite/ChronosTestCase.php
@@ -4,6 +4,23 @@ namespace Chronos\TestSuite;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class ChronosTestCase extends BaseTestCase
+class ChronosTestCase extends BaseTestCase
 {
+    protected $classOfTest;
+
+    public function testCheckAllMethodsHasTest()
+    {
+        $methodsOfClassTest = get_class_methods($this->classOfTest);
+        foreach ($methodsOfClassTest as $k => $methodOfClassTest) {
+            $method = 'test'.ucwords($methodOfClassTest);
+            $message = 'Is missing test of '.$methodOfClassTest;
+            $isCallable = is_callable([$this, $method]);
+            $this->assertTrue($isCallable, $message.'['.$method.']');
+        }
+    }
+
+    protected function setClassOfTest($classOfTest)
+    {
+        $this->classOfTest = $classOfTest;
+    }
 }
