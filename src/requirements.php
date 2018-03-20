c115cb6cccf673151bf984fa22748593e7076b87
Gean Geowle <geangeowle@gmail.com>
2018-03-20 16:59:17 -0300

    Added files for minimum requirements of application

---
+++ src/requirements.php
@@ -0,0 +1,8 @@
+<?php
+
+// You can empty out this file, if you are certain that you match all requirements.
+
+// You can remove this if you are confident that your PHP version is sufficient.
+if (version_compare(PHP_VERSION, '5.6.0') < 0) {
+    trigger_error('Your PHP version must be equal or higher than 5.6.0 to use ChronosPHP.'.PHP_EOL, E_USER_ERROR);
+}
