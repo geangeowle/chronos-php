76a92fb3b2d9a893c601a53da35339d8ce63e6e3
Gean Geowle <geangeowle@gmail.com>
2018-04-08 21:00:46 -0300

    Update ErrorController

--- src/Controllers/ErrorController.php
+++ src/Controllers/ErrorController.php
@@ -2,20 +2,34 @@
 
 namespace Chronos\Controllers;
 
-use App\Controllers\AppController;
+use Chronos\Http\Router;
+use Chronos\Utils\Configure;
+use Chronos\Utils\Inflector;
 
-final class ErrorController extends AppController
+final class ErrorController extends BaseController
 {
     public $name = 'Error';
-    public $layout = 'default_error';
     public $pageTitle = 'ChronosPHP Error';
+
+    public function initialize()
+    {
+        //$this->Page = new PageModel();
+        $this->setLayout('default_error');
+    }
 
     public function index()
     {
+        $this->set('varX', 'dataX');
     }
 
-    public function missingClass($className = '')
+    public function missingClass($className, $url)
     {
+        $dsParams = Router::parse(str_replace('\\', '/', $url), false);
+        $dsPathNamespace = Configure::read(Inflector::camelize($dsParams['url']['namespace']).'.Path');
+        $this->set('dsNamespace', $dsParams['url']['namespace']);
+        $this->set('dsPathNamespace', $dsPathNamespace);
+        $this->set('dsPath', $dsPathNamespace);
+        $this->set('dsClassName', $className);
     }
 
     public function missingMethod($methodName = '')
--- src/Views/Error/missing_class.php
+++ src/Views/Error/missing_class.php
@@ -1,2 +1,13 @@
-<h2>No route matches [GET] -> missingClass</h2>
-<p><code>ChronosPHP.root: /home/geangeowle/myapp</code></p>
+
+<h2>Missing Controller</h2>
+<p>
+  <code>
+    <?php echo sprintf('Namespace: %s', $dsNamespace); ?><br />
+    <?php echo sprintf('Path Namespace: %s', $dsPathNamespace); ?>
+  </code>
+</p>
+<p>
+  <code>
+    <?php echo sprintf('Error: Create the class %s below in file: %s/Controller/%s.php', $dsClassName, $dsPath, $dsClassName); ?>
+  </code>
+</p>
